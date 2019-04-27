00feacac3f4a1d23c2cb774d8898d317f37f31fa
安泰 李 <antaylee@msn.com>
2019-04-27 10:51:09 +0800

    初版

---
+++ OnlineGame.Web/Models/Gamer/BoardGame.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Entity;
+using System.Linq;
+namespace OnlineGame.Web.Models
+{
+    public class BoardGame
+    {
+        public Gamer GameHolder
+        {
+            get
+            {
+                using (OnlineGameContext dbContext = new OnlineGameContext())
+                {
+                    return dbContext.Gamer.SingleAsync(x => x.Id == 1).Result;
+                }
+            }
+        }
+    }
+}
